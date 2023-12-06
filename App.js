2982098bbaa42c3a4c5f0fd0f6ba86e4cf485e02
Dominik Soj <domsoj@gmail.com>
2023-12-06 16:54:13 +0100

    init

--- App.js
+++ App.js
@@ -1,20 +1,45 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from "react";
+import { StatusBar } from "expo-status-bar";
+import { View, Text, Button, StyleSheet } from "react-native";
 
-export default function App() {
+const App = () => {
+  const [counter, setCounter] = useState(0);
+
+  const incrementCounter = () => {
+    setCounter(counter + 1);
+  };
+
+  const decrementCounter = () => {
+    setCounter(counter - 1);
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.text}>Licznik: {counter}</Text>
+      <View style={styles.buttonContainer}>
+        <Button title="Zwiększ" onPress={incrementCounter} />
+        <Button title="Zmniejsz" onPress={decrementCounter} />
+      </View>
       <StatusBar style="auto" />
     </View>
   );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  text: {
+    fontSize: 24,
+    marginBottom: 20,
+  },
+  buttonContainer: {
+    flexDirection: "row",
+    justifyContent: "space-around",
+    width: "80%",
   },
 });
+
+export default App;
