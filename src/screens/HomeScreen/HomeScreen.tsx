90ac2da127ceaa718ea5ef3b5e28e935c746cb8b
Dominik Soj <48448459+WestHumble@users.noreply.github.com>
2024-02-20 20:39:28 +0100

    Adding markers

--- src/screens/HomeScreen/HomeScreen.tsx
+++ src/screens/HomeScreen/HomeScreen.tsx
@@ -1,24 +1,39 @@
 import { View, Text, StyleSheet } from "react-native";
-import React, { useRef, useEffect } from "react";
-import MapView, { PROVIDER_GOOGLE } from "react-native-maps";
+import React, { useRef, useEffect, useState } from "react";
+import MapView, { Marker, PROVIDER_GOOGLE } from "react-native-maps";
 import { useNavigation } from "@react-navigation/native";
 import CustomButton from "../../components/CustomButton";
 
 const HomeScreen = () => {
   const navigation = useNavigation();
   const mapViewRef = useRef<MapView>(null);
-  const INITIAL_REGION = {
-    latitude: 51.9194,
-    longitude: 19.1451,
-    latitudeDelta: 14,
-    longitudeDelta: 16,
-  };
+  const [region, setRegion] = useState({
+    latitude: 52.4064,
+    longitude: 16.9252,
+    latitudeDelta: 0.14,
+    longitudeDelta: 0.16,
+  });
+
+  const markers = [
+    {
+      latlng: { latitude: 52.4064, longitude: 16.9252 },
+      title: "Marker 1",
+      description: "Opis Marker 1",
+    },
+    // Dodaj więcej markerów według potrzeb
+  ];
 
   useEffect(() => {
     if (mapViewRef.current) {
-      mapViewRef.current.animateToRegion(INITIAL_REGION, 1000);
+      mapViewRef.current.animateToRegion(region, 1000);
     }
   }, []);
+
+  const onRegionChange = (newRegion) => {
+    // Dodaj dowolną logikę, jeśli potrzebujesz reagować na zmiany regionu mapy
+    // Consol loguje informacje o zmianie regionu
+    // console.log("Region changed:", newRegion);
+  };
 
   const onBackToLoginPress = () => {
     navigation.navigate("SignIn");
@@ -28,12 +43,22 @@ const HomeScreen = () => {
       <MapView
         style={styles.map}
         provider={PROVIDER_GOOGLE}
-        initialRegion={INITIAL_REGION}
+        initialRegion={region}
+        onRegionChange={onRegionChange}
         showsUserLocation={true}
         showsMyLocationButton={true}
         userInterfaceStyle="dark"
         followsUserLocation={true}
-      />
+      >
+        {markers.map((marker, index) => (
+          <Marker
+            key={index}
+            coordinate={marker.latlng}
+            title={marker.title}
+            description={marker.description}
+          />
+        ))}
+      </MapView>
       <CustomButton
         additionalStyles={{
           position: "absolute",
@@ -45,6 +70,8 @@ const HomeScreen = () => {
         text="Cofnij"
         onPress={onBackToLoginPress}
         type="PRIMARY"
+        bgColor={undefined}
+        fgColor={undefined}
       />
     </View>
   );
