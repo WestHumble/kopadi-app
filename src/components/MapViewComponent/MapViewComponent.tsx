4eacc46e19645171e3a5579744ceec239ff4df29
Dominik Soj <48448459+WestHumble@users.noreply.github.com>
2024-04-03 19:23:34 +0200

    Event tooltip details

--- src/components/MapViewComponent/MapViewComponent.tsx
+++ src/components/MapViewComponent/MapViewComponent.tsx
@@ -1,182 +1,238 @@
-import {StyleSheet} from "react-native";
-import React, {useRef, useEffect, useState, useContext} from "react";
-import MapView, {Marker, PROVIDER_GOOGLE} from "react-native-maps";
+import { StyleSheet, View, Text } from "react-native";
+import React, { useRef, useEffect, useState, useContext } from "react";
+import MapView, { Marker, PROVIDER_GOOGLE, Callout } from "react-native-maps";
 import CustomButton from "../../components/CustomButton";
-import {LatLngData, MarkerData} from "../../types/marker";
-import {LocationContext} from "../../context/LocationContext";
-import {ApiContext} from "../../context/ApiContext";
-import {EventsContext} from "../../context/EventsContext";
+import { LatLngData, MarkerData } from "../../types/marker";
+import { LocationContext } from "../../context/LocationContext";
+import { ApiContext } from "../../context/ApiContext";
+import { EventsContext } from "../../context/EventsContext";
 
 const MapViewComponent = () => {
-    const {
-        eventsCreated,
-        eventsInvited,
-        eventsOther,
-        eventsCreatedSearch,
-        eventsInvitedSearch,
-        eventsOtherSearch,
-        loadCloseEvents,
-        clearSearchEvents,
-        isSearchActive,
-    } = useContext(EventsContext);
-    const {userLocation, shareLocation, setShareLocation} =
-        useContext(LocationContext);
-    const {get, userToken} = useContext(ApiContext);
-    const mapViewRef = useRef<MapView>(null);
-    const [isUserLocationHandled, setUserLocationHandled] = useState(false);
-    const [region, setRegion] = useState({
-        latitude: userLocation?.coords.latitude ?? 52.4064,
-        longitude: userLocation?.coords.longitude ?? 16.9252,
-        latitudeDelta: 0.14,
-        longitudeDelta: 0.16,
-    });
-    useEffect(() => {
-        if (isUserLocationHandled || !userLocation)
-            return
-        setUserLocationHandled(true)
-        let regionUser = {
-            latitude: userLocation.coords.latitude,
-            longitude: userLocation.coords.longitude,
-            latitudeDelta: 0.14,
-            longitudeDelta: 0.16,
-        }
-        loadCloseEvents(regionUser)
-    }, [userLocation]);
-
-    const [markers, setMarkers] = useState<MarkerData[]>([]);
-    const [friendsMarkers, setFriendsMarkers] = useState<MarkerData[]>([]);
-
-    const onRegionChange = (newRegion) => {
-        setRegion(newRegion);
+  const {
+    eventsCreated,
+    eventsInvited,
+    eventsOther,
+    eventsCreatedSearch,
+    eventsInvitedSearch,
+    eventsOtherSearch,
+    loadCloseEvents,
+    clearSearchEvents,
+    isSearchActive,
+  } = useContext(EventsContext);
+  const { userLocation, shareLocation, setShareLocation } =
+    useContext(LocationContext);
+  const { get, userToken } = useContext(ApiContext);
+  const mapViewRef = useRef<MapView>(null);
+  const [isUserLocationHandled, setUserLocationHandled] = useState(false);
+  const [region, setRegion] = useState({
+    latitude: userLocation?.coords.latitude ?? 52.4064,
+    longitude: userLocation?.coords.longitude ?? 16.9252,
+    latitudeDelta: 0.14,
+    longitudeDelta: 0.16,
+  });
+  useEffect(() => {
+    if (isUserLocationHandled || !userLocation) return;
+    setUserLocationHandled(true);
+    let regionUser = {
+      latitude: userLocation.coords.latitude,
+      longitude: userLocation.coords.longitude,
+      latitudeDelta: 0.14,
+      longitudeDelta: 0.16,
     };
-    const onShareLocationToggle = () => {
-        setShareLocation(!shareLocation);
-    };
-    const onLoadCloseEvents = () => {
-        loadCloseEvents(region)
-    };
+    loadCloseEvents(regionUser);
+  }, [userLocation]);
 
-    useEffect(() => {
-        if (mapViewRef.current) {
-            mapViewRef.current.animateToRegion(region, 1000);
-        }
-    }, []);
+  const [markers, setMarkers] = useState<MarkerData[]>([]);
+  const [friendsMarkers, setFriendsMarkers] = useState<MarkerData[]>([]);
 
-    useEffect(() => {
-        if (isSearchActive) {
-            setMarkers(eventsCreatedSearch.concat(eventsInvitedSearch, eventsOtherSearch))
-            return
-        }
-        setMarkers(eventsCreated.concat(eventsInvited, eventsOther))
-    }, [eventsCreated, eventsInvited, eventsOther, eventsCreatedSearch, eventsInvitedSearch, eventsOtherSearch, isSearchActive]);
+  const onRegionChange = (newRegion) => {
+    setRegion(newRegion);
+  };
+  const onShareLocationToggle = () => {
+    setShareLocation(!shareLocation);
+  };
+  const onLoadCloseEvents = () => {
+    loadCloseEvents(region);
+  };
 
-
-    const updateFriendsMarkers = () => {
-        get('user/location/get-friends', null, (res) => {
-            let latlngData: LatLngData
-            let newMarkers: MarkerData[] = []
-            for (var k in res.data) {
-                latlngData = res.data[k]
-                newMarkers.push({latlng: latlngData, name: k, description: "friend"})
-            }
-            setFriendsMarkers(newMarkers)
-        })
+  useEffect(() => {
+    if (mapViewRef.current) {
+      mapViewRef.current.animateToRegion(region, 1000);
     }
+  }, []);
 
-    useEffect(() => {
-        if (!userToken) {
-            return
-        }
-        updateFriendsMarkers();
-        const interval = setInterval(() => updateFriendsMarkers(), parseInt(process.env.EXPO_PUBLIC_LOCALIZATION_UPDATE_TIME));
-        return () => clearInterval(interval);
-    }, [userToken]);
+  useEffect(() => {
+    if (isSearchActive) {
+      setMarkers(
+        eventsCreatedSearch.concat(eventsInvitedSearch, eventsOtherSearch)
+      );
+      return;
+    }
+    setMarkers(eventsCreated.concat(eventsInvited, eventsOther));
+  }, [
+    eventsCreated,
+    eventsInvited,
+    eventsOther,
+    eventsCreatedSearch,
+    eventsInvitedSearch,
+    eventsOtherSearch,
+    isSearchActive,
+  ]);
 
-    return (
-        <>
-            <MapView
-                style={styles.map}
-                provider={PROVIDER_GOOGLE}
-                initialRegion={region}
-                onRegionChange={onRegionChange}
-                showsUserLocation={true}
-                userInterfaceStyle="dark"
-                followsUserLocation={true}
-                ref={mapViewRef}
-            >
-                {markers.map((marker, index) => (
-                    <Marker
-                        key={index}
-                        coordinate={marker.latlng}
-                        title={marker.name}
-                        description={marker.description}
-                    />
-                ))}
-                {friendsMarkers.map((marker, index) => (
-                    <Marker
-                        key={index}
-                        coordinate={marker.latlng}
-                        title={marker.name}
-                        description={marker.description}
-                    />
-                ))}
-            </MapView>
-            <CustomButton
-                additionalStyles={{
-                    position: "absolute",
-                    top: "70%",
-                    left: "35%",
-                    width: "20%",
-                    marginHorizontal: "5%",
-                }}
-                text={shareLocation ? "Sharing on" : "Sharing off"}
-                onPress={onShareLocationToggle}
-                type="PRIMARY"
-                bgColor={undefined}
-                fgColor={undefined}
-            />
-            {isSearchActive && (
-                <CustomButton
-                    additionalStyles={{
-                        position: "absolute",
-                        top: "70%",
-                        left: "55%",
-                        width: "20%",
-                        marginHorizontal: "5%",
-                    }}
-                    text="Clear search"
-                    onPress={clearSearchEvents}
-                    type="PRIMARY"
-                    bgColor={undefined}
-                    fgColor={undefined}
-                />
-            )}
-            <CustomButton
-                additionalStyles={{
-                    position: "absolute",
-                    top: "70%",
-                    left: "5%",
-                    width: "20%",
-                    marginHorizontal: "5%",
-                }}
-                text="Load close events"
-                onPress={onLoadCloseEvents}
-                type="PRIMARY"
-                bgColor={undefined}
-                fgColor={undefined}
-            />
-        </>
+  const updateFriendsMarkers = () => {
+    get("user/location/get-friends", null, (res) => {
+      let latlngData: LatLngData;
+      let newMarkers: MarkerData[] = [];
+      for (var k in res.data) {
+        latlngData = res.data[k];
+        newMarkers.push({ latlng: latlngData, name: k, description: "friend" });
+      }
+      setFriendsMarkers(newMarkers);
+    });
+  };
+
+  useEffect(() => {
+    if (!userToken) {
+      return;
+    }
+    updateFriendsMarkers();
+    const interval = setInterval(
+      () => updateFriendsMarkers(),
+      parseInt(process.env.EXPO_PUBLIC_LOCALIZATION_UPDATE_TIME)
     );
+    return () => clearInterval(interval);
+  }, [userToken]);
+
+  return (
+    <>
+      <MapView
+        style={styles.map}
+        provider={PROVIDER_GOOGLE}
+        initialRegion={region}
+        onRegionChange={onRegionChange}
+        showsUserLocation={true}
+        userInterfaceStyle="dark"
+        followsUserLocation={true}
+        ref={mapViewRef}
+      >
+        {markers.map((marker, index) => (
+          <Marker
+            key={index}
+            coordinate={marker.latlng}
+            title={marker.name}
+            description={marker.description}
+          >
+            <Callout tooltip style={styles.callout}>
+              <View>
+                <Text style={styles.title}>{marker.name}</Text>
+                <Text style={styles.description}>{marker.description}</Text>
+                <CustomButton
+                  text="Details"
+                  onPress={() => {}}
+                  type="PRIMARY"
+                  bgColor={undefined}
+                  fgColor={undefined}
+                  additionalStyles={undefined}
+                />
+              </View>
+            </Callout>
+          </Marker>
+        ))}
+        {friendsMarkers.map((marker, index) => (
+          <Marker
+            key={index}
+            coordinate={marker.latlng}
+            title={marker.name}
+            description={marker.description}
+          >
+            <Callout tooltip style={styles.callout}>
+              <View>
+                <Text style={styles.title}>{marker.name}</Text>
+                <Text style={styles.description}>{marker.description}</Text>
+                <CustomButton
+                  text="Details"
+                  onPress={() => {}}
+                  type="PRIMARY"
+                  bgColor={undefined}
+                  fgColor={undefined}
+                  additionalStyles={undefined}
+                />
+              </View>
+            </Callout>
+          </Marker>
+        ))}
+      </MapView>
+      <CustomButton
+        additionalStyles={{
+          position: "absolute",
+          top: "70%",
+          left: "35%",
+          width: "20%",
+          marginHorizontal: "5%",
+        }}
+        text={shareLocation ? "Sharing on" : "Sharing off"}
+        onPress={onShareLocationToggle}
+        type="PRIMARY"
+        bgColor={undefined}
+        fgColor={undefined}
+      />
+      {isSearchActive && (
+        <CustomButton
+          additionalStyles={{
+            position: "absolute",
+            top: "70%",
+            left: "55%",
+            width: "20%",
+            marginHorizontal: "5%",
+          }}
+          text="Clear search"
+          onPress={clearSearchEvents}
+          type="PRIMARY"
+          bgColor={undefined}
+          fgColor={undefined}
+        />
+      )}
+      <CustomButton
+        additionalStyles={{
+          position: "absolute",
+          top: "70%",
+          left: "5%",
+          width: "20%",
+          marginHorizontal: "5%",
+        }}
+        text="Load close events"
+        onPress={onLoadCloseEvents}
+        type="PRIMARY"
+        bgColor={undefined}
+        fgColor={undefined}
+      />
+    </>
+  );
 };
 
 const styles = StyleSheet.create({
-    map: {
-        width: "100%",
-        height: "100%",
-        position: "absolute",
-        zIndex: -1,
-        elevation: 1,
-    },
+  map: {
+    width: "100%",
+    height: "100%",
+    position: "absolute",
+    zIndex: -1,
+    elevation: 1,
+  },
+  callout: {
+    backgroundColor: "#131417",
+    width: 200,
+    borderRadius: 20,
+    padding: 10,
+  },
+  title: {
+    color: "#fff",
+    fontWeight: "bold",
+    fontSize: 16,
+  },
+  description: {
+    color: "#fff",
+    fontSize: 14,
+  },
 });
 
 export default MapViewComponent;
