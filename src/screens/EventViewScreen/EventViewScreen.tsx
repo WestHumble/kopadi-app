4b989d02ad58cd53dbf39f17d4271172b23e2d34
Dominik Soj <48448459+WestHumble@users.noreply.github.com>
2024-04-16 19:42:24 +0200

    Event view - UI fix

--- src/screens/EventViewScreen/EventViewScreen.tsx
+++ src/screens/EventViewScreen/EventViewScreen.tsx
@@ -2,6 +2,7 @@ import {
   View,
   Text,
   StyleSheet,
+  Image,
   useWindowDimensions,
   ScrollView,
   ActivityIndicator,
@@ -15,6 +16,7 @@ import { ApiContext } from "../../context/ApiContext";
 import { EventsContext } from "../../context/EventsContext";
 import { useNavigation } from "@react-navigation/native";
 import { Event } from "../../types/event";
+import eventImg from "../../../assets/images/emoji-beer-mug.png";
 
 Geocoding.init(process.env.REACT_APP_GEOCODING_API_KEY);
 
@@ -41,6 +43,7 @@ const EventViewScreen = ({ route }) => {
   const showDatePicker = () => {
     setDatePickerVisibility(true);
   };
+  const onPressJoinEvent = () => {};
 
   const hideDatePicker = () => {
     setDatePickerVisibility(false);
@@ -64,10 +67,25 @@ const EventViewScreen = ({ route }) => {
     <>
       <View style={[styles.root, { height: height * 1 }]}>
         <View style={styles.windowTab}>
-          <Text style={styles.title} resizeMode="contain">
-            {event.name}
+          <View style={styles.HeaderSection}>
+            <Image
+              source={eventImg}
+              style={styles.eventImg}
+              resizeMode="contain"
+            />
+            <View style={styles.HeaderSectionText}>
+              <Text style={styles.titleSectionHeader} resizeMode="contain">
+                Nazwa wydarzenia
+              </Text>
+              <Text style={styles.title} resizeMode="contain">
+                {event.name}
+              </Text>
+            </View>
+          </View>
+          <Text style={styles.titleSectionDate} resizeMode="contain">
+            Data i godzina
           </Text>
-          <Text style={styles.title} resizeMode="contain">
+          <Text style={styles.date} resizeMode="contain">
             {new Date(event.date.date).toLocaleString([], {
               year: "numeric",
               month: "numeric",
@@ -76,14 +94,34 @@ const EventViewScreen = ({ route }) => {
               minute: "2-digit",
             })}
           </Text>
-          <Text style={styles.title} resizeMode="contain">
+          <View style={styles.hr} />
+          <Text style={styles.titleSectionAddress} resizeMode="contain">
+            Lokalizacja wydarzenia
+          </Text>
+          <Text style={styles.address} resizeMode="contain">
             {event.address}
           </Text>
-          <Text style={styles.title} resizeMode="contain">
+          <View style={styles.hr} />
+          <Text style={styles.titleSectionDescription} resizeMode="contain">
+            Opis wydarzenia
+          </Text>
+          <Text style={styles.description} resizeMode="contain">
             {event.description}
           </Text>
 
-          <ScrollView showsVerticalScrollIndicator={false}></ScrollView>
+          <CustomButton
+            additionalStyles={{
+              margin: 0,
+            }}
+            additionalStylesText={{
+              fontSize: 20,
+            }}
+            text="Dołącz do wydarzenia"
+            onPress={onPressJoinEvent}
+            type="PRIMARY"
+            bgColor={undefined}
+            fgColor={undefined}
+          />
         </View>
       </View>
     </>
@@ -113,13 +151,6 @@ const styles = StyleSheet.create({
   link: {
     color: "#003f63",
   },
-  title: {
-    fontSize: 24,
-    fontWeight: "bold",
-    marginBottom: 10,
-    marginTop: "5%",
-    color: "#fff",
-  },
   inputDate: {
     color: "#fff",
   },
@@ -141,6 +172,80 @@ const styles = StyleSheet.create({
   },
   inputEventName: {
     color: "#fff",
+  },
+  eventImg: {
+    width: 70,
+    height: 70,
+    alignSelf: "left",
+    marginBottom: 0,
+    overflow: "visible",
+    zIndex: 1,
+  },
+  HeaderSection: {
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "flex-start",
+    backgroundColor: "#131417",
+    padding: 20,
+    borderRadius: 20,
+  },
+  HeaderSectionText: {
+    marginLeft: 20,
+    flexDirection: "column",
+    alignItems: "flex-start",
+    justifyContent: "flex-end",
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: "bold",
+    marginTop: 5,
+    color: "#fff",
+  },
+  date: {
+    color: "#fff",
+    fontSize: 24,
+    marginBottom: 20,
+    marginTop: 5,
+  },
+  address: {
+    color: "#fff",
+    fontSize: 24,
+    marginBottom: 20,
+    marginTop: 5,
+  },
+  description: {
+    color: "#fff",
+    fontSize: 24,
+    marginBottom: 20,
+    marginTop: 5,
+  },
+  hr: {
+    borderBottomColor: "#676D75",
+    borderBottomWidth: 1,
+    marginVertical: 10,
+  },
+  titleSectionHeader: {
+    fontSize: 14,
+    fontWeight: "bold",
+    color: "#F2B138",
+  },
+  titleSectionDate: {
+    fontSize: 14,
+    fontWeight: "bold",
+    color: "#F2B138",
+    marginTop: 20,
+  },
+  titleSectionAddress: {
+    fontSize: 14,
+    fontWeight: "bold",
+    color: "#F2B138",
+    marginTop: 20,
+  },
+  titleSectionDescription: {
+    fontSize: 14,
+    fontWeight: "bold",
+    color: "#F2B138",
+    marginTop: 20,
   },
 });
 
