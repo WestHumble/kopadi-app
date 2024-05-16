13d2f749d422600392e3c918e0f8d600650d1314
Dominik Soj <48448459+WestHumble@users.noreply.github.com>
2024-05-16 20:04:35 +0200

    refactor: Update NewChatScreen component

--- src/screens/NewChatScreen/NewChatScreen.tsx
+++ src/screens/NewChatScreen/NewChatScreen.tsx
@@ -3,16 +3,18 @@ import {
   Text,
   StyleSheet,
   useWindowDimensions,
-  ScrollView, ActivityIndicator, Alert,
+  ScrollView,
+  ActivityIndicator,
+  Alert,
 } from "react-native";
-import React, {useContext, useEffect, useState} from "react";
+import React, { useContext, useEffect, useState } from "react";
 import CustomInput from "../../components/CustomInput";
-import {FriendsContext} from "../../context/FriendsContext";
+import { FriendsContext } from "../../context/FriendsContext";
 import FriendList from "../../components/FriendList";
-import {Friend} from "../../types/friend";
+import { Friend } from "../../types/friend";
 import CustomButton from "../../components/CustomButton";
-import {ChatContext} from "../../context/ChatContext";
-import {useIsFocused} from "@react-navigation/native";
+import { ChatContext } from "../../context/ChatContext";
+import { useIsFocused } from "@react-navigation/native";
 
 const NewChatScreen = () => {
   const { height } = useWindowDimensions();
@@ -25,78 +27,102 @@ const NewChatScreen = () => {
     setPickedFriends([...pickedFriends, friend]);
   };
   const removeFriend = (friend: Friend) => {
-    setPickedFriends(pickedFriends.filter(f => f.id !== friend.id));
+    setPickedFriends(pickedFriends.filter((f) => f.id !== friend.id));
   };
   const onInitChatPressed = () => {
     if (pickedFriends.length === 0) {
-      Alert.alert("Wybierz przynajmniej jednego znajomego")
-      return
+      Alert.alert("Wybierz przynajmniej jednego znajomego");
+      return;
     }
     initChat(pickedFriends);
-    setPickedFriends([])
+    setPickedFriends([]);
   };
-  const isFocused = useIsFocused()
+  const isFocused = useIsFocused();
 
   useEffect(() => {
     if (isFocused) {
-      setPickedFriends([])
+      setPickedFriends([]);
     }
-  }, [isFocused])
+  }, [isFocused]);
   const data = [
     {
       title: "Dodaj znajomych",
-      data: searchPhrase ? friends.filter(friend => {
-        let matches = true;
-        searchPhrase.split(' ').every(phrasePart => {
-          if (!friend.name.toLowerCase().startsWith(phrasePart.toLowerCase()) &&
-              !friend.surname.toLowerCase().startsWith(phrasePart.toLowerCase())) {
-            matches = false
-            return false
-          }
-          return true
-        })
-        return matches
-      }) : friends,
+      data: searchPhrase
+        ? friends.filter((friend) => {
+            let matches = true;
+            searchPhrase.split(" ").every((phrasePart) => {
+              if (
+                !friend.name
+                  .toLowerCase()
+                  .startsWith(phrasePart.toLowerCase()) &&
+                !friend.surname
+                  .toLowerCase()
+                  .startsWith(phrasePart.toLowerCase())
+              ) {
+                matches = false;
+                return false;
+              }
+              return true;
+            });
+            return matches;
+          })
+        : friends,
     },
   ];
 
   if (initializingChat) {
     return (
-        <View style={{ flex: 1, justifyContent: "center",
-          backgroundColor: "#131417",alignItems: "center" }}>
-          <ActivityIndicator size={"large"} />
-        </View>
+      <View
+        style={{
+          flex: 1,
+          justifyContent: "center",
+          backgroundColor: "#131417",
+          alignItems: "center",
+        }}
+      >
+        <ActivityIndicator size={"large"} />
+      </View>
     );
   }
 
   return (
-      <>
-        <View style={[styles.root, { height: height * 1 }]}>
-          <View style={styles.windowTab}>
-            <FriendList
-                noDataText={"Brak znajomych"}
-                data={data}
-              action={friend=>{pickedFriends.find(e => e.id === friend.id) ? removeFriend(friend) : pickFriend(friend)}}
-              actionText={friend=>{ return pickedFriends.find(e => e.id === friend.id) ? "Usuń" : "Dodaj"}}
-            />
+    <>
+      <View style={[styles.root, { height: height * 1 }]}>
+        <View style={styles.windowTab}>
+          <FriendList
+            noDataText={"Brak znajomych"}
+            data={data}
+            action={(friend) => {
+              pickedFriends.find((e) => e.id === friend.id)
+                ? removeFriend(friend)
+                : pickFriend(friend);
+            }}
+            actionText={(friend) => {
+              return pickedFriends.find((e) => e.id === friend.id)
+                ? "Usuń"
+                : "Dodaj";
+            }}
+          />
+          <View style={styles.searchContainer}>
             <CustomInput
-                placeholder="Szukaj"
-                value={searchPhrase}
-                setValue={setSearchPhrase}
-                secureTextEntry={undefined}
-                additionalStyle={styles.searchInput}
+              placeholder="Szukaj"
+              value={searchPhrase}
+              setValue={setSearchPhrase}
+              secureTextEntry={undefined}
+              additionalStyle={styles.searchInput}
             />
             <CustomButton
-                text="+"
-                onPress={onInitChatPressed}
-                type="PRIMARY"
-                bgColor={undefined}
-                fgColor={undefined}
-                additionalStyles={styles.newChatButton}
+              text="Utwórz czat"
+              onPress={onInitChatPressed}
+              type="PRIMARY"
+              bgColor={undefined}
+              fgColor={undefined}
+              additionalStyles={styles.newChatButton}
             />
           </View>
         </View>
-      </>
+      </View>
+    </>
   );
 };
 
@@ -131,17 +157,20 @@ const styles = StyleSheet.create({
     color: "#fff",
   },
   newChatButton: {
-    position: "absolute",
-    bottom: "16%",
-    right: "5%",
-    borderWidth: 3,
     fontSize: 50,
-    width: 55,
-    height: 55,
-    borderRadius: 50,
-    zIndex: 99,
+    flex: 1 / 3,
   },
-  searchInput: {color: "#fff"},
+  searchInput: {
+    color: "#fff",
+    flex: 1,
+    marginRight: 10,
+  },
+  searchContainer: {
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "center",
+    marginTop: 0,
+  },
 });
 
 export default NewChatScreen;
